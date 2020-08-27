0d2dfca2d48df4f7bdc8e4461bdc216c12a16fb1
Xiaojun Ma <you@example.com>
2020-08-27 20:27:15 +1000

    week5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { AccountComponent } from './account/account.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { AccountComponent } from "./account/account.component";
+import { ProfileComponent } from "./profile/profile.component";
 
 const routes: Routes = [
-  {path:"login", component:LoginComponent},
-  {path:"account", component:AccountComponent}
+  { path: "login", component: LoginComponent },
+  { path: "account/:user", component: AccountComponent },
+  { path: "profile", component: ProfileComponent }
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
